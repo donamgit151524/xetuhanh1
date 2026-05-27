9f96b409b05739aedbb34649f83961dfe7f9fe13
donamgit151524 <email_github_cua_ban@example.com>
2026-05-27 22:08:34 +0700

    Dang loi gazebo

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,46 +2,61 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
 
+    # 1. Khai báo các biến tên gói và tên robot để dễ dàng thay đổi sau này
+    package_name = 'xetuhanh1'
+    robot_name = 'Sieu_xe_AGV'
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+    # Khởi tạo Launch Configuration cho môi trường (world)
+    world = LaunchConfiguration('world')
 
-    ten_goi_goi = 'xetuhanh1' #<--- CHANGE ME
-
-    phien_toi_rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(ten_goi_goi),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    # 2. Định nghĩa tham số world (giống tư duy của tác giả)
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value='', # Để trống mặc định sẽ tải world trống của Gazebo
+        description='Đường dẫn đến file .world của Gazebo'
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    bao_gom_gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+    # Chạy file rsp.launch.py từ package của bạn
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    khoi_tao_thuc_the = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'Sieu_xe_AGV'],
-                        output='screen')
+    # Khởi chạy Gazebo Classic (tối ưu: truyền thêm tham số world)
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'
+        )]),
+        launch_arguments={'world': world}.items()
+    )
 
+    # 3. Khởi tạo robot (tối ưu: thêm toạ độ -z 0.1)
+    spawn_entity = Node(
+        package='gazebo_ros', 
+        executable='spawn_entity.py',
+        arguments=[
+            '-topic', 'robot_description',
+            '-entity', robot_name,
+            '-z', '0.1'  # Thả robot cách mặt đất 0.1m để tránh kẹt bánh xe
+        ],
+        output='screen'
+    )
 
-
-    # Launch them all!
+    # Chạy tất cả các tiến trình
     return LaunchDescription([
-        phien_toi_rsp,
-        bao_gom_gazebo,
-        khoi_tao_thuc_the,
+        world_arg,
+        rsp,
+        gazebo,
+        spawn_entity,
     ])
