c25036a3300dccbe103535cb9385206daebe1900
donamgit151524 <email_github_cua_ban@example.com>
2026-05-28 09:15:47 +0700

    TAMJ  OK

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -51,11 +51,13 @@ def generate_launch_description():
         package='twist_mux',
         executable='twist_mux',
         output='screen',
-        remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel')], # <--- Sửa ở đây
+        # Đã thêm _unstamped vào đuôi
+        remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')], 
         parameters=[
             {'use_sim_time': True},
             twist_mux_config])
-    
+
+            
     return LaunchDescription([
         rsp,
         world_arg,
